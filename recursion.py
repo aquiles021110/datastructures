1babf2f1be852b1867ac5cd18aca82d327cf5460
aquiles021110 <aquiles@bousigues.com>
2026-01-14 15:25:02 +0100

    Refactor factorial and add square function
    Added a square function to calculate the square of a number.

--- recursion.py
+++ recursion.py
@@ -4,3 +4,16 @@ def factorial(n):
     else:
         return n*factorial(n-1)
 print(factorial(7))
+
+def square(x,y=0,z=0):
+    if y==0 and z==0:
+        z+=x
+        y+=1
+        return square(x,y,z)
+    if y<x:
+        z+=x
+        y+=1
+        return square(x,y,z)
+    if x==y:
+        return z
+print(square())
